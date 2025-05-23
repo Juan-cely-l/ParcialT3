b5b5e4754dcb6ef68f200c212362d0d85657d886
unknown <juan.cely-l@labinfo.is.escuelaing.edu.co>
2025-05-23 15:21:13 -0500

    estructura Back

---
+++ src/main/java/edu/eci/cvds/ParcialT3/ParcialT3Application.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.ParcialT3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialT3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialT3Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/cvds/ParcialT3/ParcialT3ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.ParcialT3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialT3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
